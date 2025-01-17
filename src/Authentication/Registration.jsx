6f90358fc3e3fe9fad8c5cef00518878b50e6caa
Prajesh Kotian <prajeshkotian@gmail.com>
2025-01-16 21:28:23 -0500

    Added RegistrationPage

---
+++ src/Authentication/Registration.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import "./Registration.css";
+
+function RegistrationPage() {
+  const [formData, setFormData] = useState({ email: "", password: "" });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form submitted:", formData);
+
+  };
+
+  return (
+    <div className="container">
+      <form onSubmit={handleSubmit} className="form">
+        <h2 className="form-title">Register</h2>
+        <div className="form-group">
+          <label htmlFor="email" className="form-label">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+            className="form-input"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password" className="form-label">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+            className="form-input"
+          />
+        </div>
+        <button type="submit" className="form-button">
+          Register
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default RegistrationPage;
