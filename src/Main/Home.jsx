1fa3a38b400c947ca17138649cb195ee53a75731
Meher Suneel <suneelmeesalameher@gmail.com>
2025-01-21 17:49:07 -0500

    Update Home.jsx

--- src/Main/Home.jsx
+++ src/Main/Home.jsx
@@ -27,7 +27,7 @@ const Home = () => {
 
       <div className="wave-container">
         <svg viewBox="0 0 500 100" preserveAspectRatio="none">
-          <path d="M0,30 C150,100 350,0 500,30 L500,0 L0,0 Z" fill="#090808" />
+          <path d="M0,30 C150,100 350,0 500,30 L500,0 L0,0 Z" fill="#000000" />
         </svg>
         <h1 className="summary">
           StudyBuddy is a dynamic platform designed to help students connect with study partners based on shared interests, availability, and study goals. Whether you're looking for someone to review notes, discuss concepts, or collaborate on assignments, StudyBuddy makes it easy to find a partner who fits your schedule and subject preferences. Our platform ensures a safe and supportive learning environment, offering tools to help you stay focused, stay organized, and achieve your academic goals together. Study smarter, not harder, with StudyBuddy!
